2fd3be1e3a4a716a54d9badbe2b2eacd9971d793
AmaliaTadevosyan <49729766+AmaliaTadevosyan@users.noreply.github.com>
2023-07-09 18:05:12 +0400

    Add files via upload

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,13 +1,49 @@
+#include <iostream>
 #include <fstream>
+#include <string>
 
-// std::string program(std::string content, std::string s1, std::string s2)
-// {
-
-// }
-
-int main (int argc, char **argv)
+void replaceInFile(const std::string& filename, const std::string& s1, const std::string& s2) 
 {
-    std::string infile = argv[1];
-    std::string outfile = infile + ".replace";
-    std::
+    std::ifstream inputFile(filename);
+    if (!inputFile) {
+        std::cerr << "Failed to open input file." << std::endl;
+        return;
+    }
+
+    std::ofstream outputFile(filename + ".replace");
+    if (!outputFile) {
+        std::cerr << "Failed to create output file." << std::endl;
+        inputFile.close();
+        return;
+    }
+
+    std::string line;
+    while (getline(inputFile, line)) {
+        size_t pos = line.find(s1);
+        while (pos != std::string::npos) {
+            line.replace(pos, s1.length(), s2);
+            pos = line.find(s1, pos + s2.length());
+        }
+        outputFile << line << std::endl;
+    }
+
+    inputFile.close();
+    outputFile.close();
+    std::cout << "File replacement completed successfully." << std::endl;
+}
+
+int main(int argc, char* argv[]) 
+{
+    if (argc != 4) {
+        std::cout << "Usage: ./program <filename> <s1> <s2>" << std::endl;
+        return 0;
+    }
+
+    std::string filename = argv[1];
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+
+    replaceInFile(filename, s1, s2);
+
+    return 0;
 }
